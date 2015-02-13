595203950c82e1aa68e4096dde4aca989e12011a
Tolnai Zoltán <tolnai.zoltan@ferling.hu>
2015-02-13 08:59:28 +0100

    nos

--- index.py
+++ index.py
@@ -1,15 +1,20 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import os
+from flask import Flask
 import re
 import soupselect
 from BeautifulSoup import BeautifulSoup as Soup
 from soupselect import select
 import urllib2
-response = urllib2.urlopen('http://www.tukebusz.hu/menetrend/3/naptipus/munkanap/megallo1/103')
-html = response.read()
 
-def no_html(strin):
-	return re.sub('<[^<]+?>', '', strin)
+app = Flask(__name__)
 
-soup = Soup(html)
-print no_html(str(select(soup, '.menetrend_varhato_idopontok')))
+@app.route('/')
+def index():
+    response = urllib2.urlopen('http://www.tukebusz.hu/menetrend/3/naptipus/munkanap/megallo1/103')
+    html = response.read()
+
+    def no_html(strin):
+    	return re.sub('<[^<]+?>', '', strin)
+
+    soup = Soup(html)
+    return no_html(str(select(soup, '.menetrend_varhato_idopontok')))

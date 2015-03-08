647dc89f962232257f461f9955fa421fc71781d3
Tolnai Zoltán <tolnai.zoltan@ferling.hu>
2015-03-08 17:37:30 +0100

    automatikus frissítés, visszaszámlálás gps adatok alapján

--- harmasbusz/harmas.py
+++ harmasbusz/harmas.py
@@ -4,14 +4,35 @@ import soupselect
 from bs4 import BeautifulSoup
 from soupselect import select
 import urllib2
+import urllib
+import datetime
 
 class Harmas:
-	def get(self):
-		menetrend_url = 'http://www.tukebusz.hu/menetrend/3/naptipus/munkanap/megallo1/103'
+	def idopontok(self):
+		day = datetime.datetime.today().weekday()
+		if day == 6:
+			naptipus = "szabadnap"
+		elif day == 4:
+			naptipus = "utolso"
+		else:
+			naptipus = "munkanap"
+
+		menetrend_url = 'http://www.tukebusz.hu/menetrend/3/naptipus/'+naptipus+'/megallo1/103'
 		response = urllib2.urlopen(menetrend_url)
 		html = response.read()
 		soup = BeautifulSoup(html)
 		return Harmas.remove_html(str(select(soup, '.menetrend_varhato_idopontok')[0]))
+
+	def kovetkezo_erkezes(self):
+		menetrend_url = 'http://menobusz.tukebusz.hu/php/sql.php'
+		data = urllib.urlencode({'id' : 'online_erkezes|27907'})
+		response = urllib2.urlopen(menetrend_url, data)
+		for x in response.read().split("\n"):
+			sor = x.split("|")
+			if sor[0] == '860':
+				kov_p = int(float(sor[3]))
+				kov_m = int((float(sor[3]) - kov_p ) * 60)
+				return [kov_p,kov_m]
 
 	@staticmethod
 	def remove_html(string):
--- harmasbusz/views.py
+++ harmasbusz/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext, loader
 from harmas import Harmas
-
+import json
 def index(request):
 	h = Harmas()
-	harmas = h.get()
-	return render(request,'harmasbusz/index.html',{ "harmas": harmas })
+	idopontok = h.idopontok()
+	kovetkezo = h.kovetkezo_erkezes()
+	return render(request,'harmasbusz/index.html',{
+		"idopontok": idopontok,
+		"kovetkezo_p": kovetkezo[0],
+		"kovetkezo_m": kovetkezo[1]
+	})
+def update(request):
+	h = Harmas()
+	kovetkezo = h.kovetkezo_erkezes()
+	json_data = json.dumps(kovetkezo)
+	return HttpResponse(json_data)
